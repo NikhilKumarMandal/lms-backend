0eab89bf7152c396a99d4c83e5769bf1600cc78f
Nikhil <kumarnikhil48578@gmail.com>
2025-05-12 22:42:05 +0530

    update

--- routes/auth-routes/index.js
+++ routes/auth-routes/index.js
@@ -7,9 +7,11 @@ const authenticateMiddleware = require("../../middleware/auth-middleware");
 const router = express.Router();
 
 router.post("/register", registerUser);
+
 router.post("/login", loginUser);
 router.get("/check-auth", authenticateMiddleware, (req, res) => {
   const user = req.user;
+  
 
   res.status(200).json({
     success: true,
