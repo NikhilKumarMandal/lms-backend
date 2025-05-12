6b211d1306f117674f9e9524628df8d441d500b1
Nikhil <kumarnikhil48578@gmail.com>
2025-05-12 23:16:04 +0530

    update

--- server.js
+++ server.js
@@ -14,13 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(
-  cors({
-    origin: "https://lms-ui-gamma.vercel.app",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
